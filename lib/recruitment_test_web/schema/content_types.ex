2f9411919c29903e5ee7c074c6b9f2d59c519934
henriqueSpencer <henriquespencer11@gmail.com>
2025-02-09 19:00:52 -0300

    feat: database connection in API route

--- lib/recruitment_test_web/schema/content_types.ex
+++ lib/recruitment_test_web/schema/content_types.ex
@@ -4,4 +4,14 @@ defmodule RecruitmentTestWeb.Schema.ContentTypes do
   object :hello_world do
     field :message, :string
   end
+
+  object :enterprise do
+    field :id, non_null(:id)
+    field :name, non_null(:string)
+    field :commercial_name, :string
+    field :cnpj, :string
+    field :description, :string
+    # field :inserted_at, :naive_datetime
+    # field :updated_at, :naive_datetime
+  end
 end
--- lib/recruitment_test_web/schema/schema.ex
+++ lib/recruitment_test_web/schema/schema.ex
@@ -4,13 +4,6 @@ defmodule RecruitmentTestWeb.Schema do
 
   alias RecruitmentTestWeb.Resolvers
 
-  object :enterprise do
-    field :id, non_null(:id)
-    field :name, non_null(:string)
-    field :commercial_name, :string
-    field :cnpj, :string
-    field :description, :string
-  end
 
   query do
     @desc "Returns a Hello World message"
@@ -21,26 +14,9 @@ defmodule RecruitmentTestWeb.Schema do
     @desc "Get an enterprise by ID"
     field :enterprise, :enterprise do
       arg :id, non_null(:id)
-      resolve(fn _, %{id: id}, _ ->
-        # Simulação de uma busca no banco de dados
-        enterprise = get_enterprise(id)
-
-        case enterprise do
-          nil -> {:error, "Enterprise not found"}
-          _ -> {:ok, enterprise}
-        end
-      end)
+      resolve(&Resolvers.Enterprise.get/3)
     end
 
-    # Simulação de um banco de dados em memória
-    defp get_enterprise(id) do
-      mock_data = [
-        %{id: "1", name: "Tech Solutions", commercial_name: "TechSol", cnpj: "12345678000190", description: "Empresa de tecnologia"},
-        %{id: "2", name: "Green Corp", commercial_name: "Green", cnpj: "98765432000112", description: "Soluções ambientais"}
-      ]
-
-      Enum.find(mock_data, fn enterprise -> enterprise.id == to_string(id) end)
-    end
 
   end
 
